56773c1d674abd3509aee866d6af93ebd1335fde
Crowdbotics <team@crowdbotics.com>
2023-09-05 23:32:04 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "yellow-fog-43241",
+  url: "https://yellow-fog-43241.botics.co",
+  api: "https://yellow-fog-43241.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
